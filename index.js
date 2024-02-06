5cb75073c08e6ae2200e211dc501e6e6b80b3a92
Rey <rsantelli@luc.edu>
2024-02-05 18:26:36 -0600

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+// Function to find matching drivers by name
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+  // Function to find drivers if beginning provided letters match
+  function fuzzyMatch(drivers, testString) {
+    return drivers.filter(driver => driver.startsWith(testString));
+  }
+  
+  // Function to check if name matches within a data structure
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
